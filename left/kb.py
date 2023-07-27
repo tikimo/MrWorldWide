0de7671d37885fa46bc42bc87eb1f927f3668d93
Tijam Moradi <tijam.moradi@cgi.com>
2023-07-27 12:40:19 +0300

    init

---
+++ left/kb.py
@@ -0,0 +1,58 @@
+import board
+
+from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
+from kmk.scanners import DiodeOrientation
+
+
+class KMKKeyboard(_KMKKeyboard):
+    col_pins = (
+        board.GP8,
+        board.GP7,
+        board.GP6,
+        board.GP5,
+        board.GP4,
+        board.GP3,
+        board.GP2,
+    )
+    row_pins = (
+        board.GP15,
+        board.GP14,
+        board.GP13,
+        board.GP12,
+        board.GP11,
+        board.GP10,
+        board.GP9,
+    )
+
+    # OLED
+    SCL = board.GP27
+    SDA = board.GP26
+    
+    diode_orientation = DiodeOrientation.COL2ROW
+    
+    # Reversed from right
+    uart_rx = board.GP0
+    uart_tx = board.GP1
+
+    coord_mapping = [
+        0,  1,  2,  3,  4,  5,  6,             55, 54, 53, 52, 51, 50, 49,
+        7,  8,  9,  10, 11, 12, 13,            62, 61, 60, 59, 58, 57, 56,
+        14, 15, 16, 17, 18, 19, 20,            69, 68, 67, 66, 65, 64, 63,
+        21, 22, 23, 24, 25, 26, 27,            76, 75, 74, 73, 72, 71, 70,
+        28, 29, 30, 31, 32, 33, 34,            83, 82, 81, 80, 79, 78, 77,
+                    36, 37, 38, 39, 40, 41,            86, 85,
+                                    47, 48,    90, 89, 88, 87, 
+    ]
+    
+    # coord_mapping = [
+    #     0,  1,  2,  3,  4,  5,  6,            55, 54, 53, 52, 51, 50, 49,
+    #     7,  8,  9, 10, 11, 12, 13,            62, 61, 60, 59, 58, 57, 56,
+    #     14, 15, 16, 17, 18, 19, 20,           69, 68, 67, 66, 65, 64, 63,
+    #     21, 22, 23, 24, 25, 26, 27,           76, 75, 74, 73, 72, 71, 70,
+    #     28, 29, 30, 31, 32, 33, 34,           83, 82, 81, 80, 79, 78, 77,
+    #                 38, 39,                           88, 87,
+    #                             40, 41,   90, 89,
+    #                             47, 48,   97, 96,
+    #                             45, 46,   95, 94
+    # ]
+
---
+++ left/main.py
@@ -0,0 +1,54 @@
+import board
+
+from kb import KMKKeyboard
+
+from kmk.extensions.media_keys import MediaKeys
+from kmk.keys import KC
+from kmk.modules.layers import Layers
+from kmk.modules.split import Split, SplitType, SplitSide
+from kmk.modules.mouse_keys import MouseKeys
+from kmk.extensions.peg_oled_display import Oled,OledDisplayMode,OledReactionType,OledData
+
+keyboard = KMKKeyboard()
+
+keyboard.debug_enabled = True
+
+keyboard.extensions.append(MediaKeys())
+keyboard.modules.append(Layers())
+keyboard.modules.append(MouseKeys())
+
+# SPLIT (BLE not supported on Circuitpython yet with PICO W, however, Micropython does support it)
+split = Split(
+    data_pin=board.GP1,
+    debug_enabled=True,
+    )
+keyboard.modules.append(split)
+
+keyboard.keymap = [
+    [   #0
+        KC.ESC,   KC.F1,    KC.F2,   KC.F3,   KC.F4,   KC.F5,     KC.F6,                            KC.F7,     KC.F8,     KC.F9,     KC.F10,    KC.F11,    KC.F12,    KC.PGUP,
+        KC.TRNS,  KC.N1,    KC.N2,   KC.N3,   KC.N4,   KC.N5,     KC.N6,                            KC.N7,     KC.N8,     KC.N9,     KC.N0,     KC.SLSH,   KC.GRAVE,  KC.PGDN,
+        KC.TAB,   KC.Q,     KC.W,    KC.E,    KC.R,    KC.T,      KC.Y,                             KC.U,      KC.I,      KC.O,      KC.P,      KC.A,      KC.CIRC,   KC.TRNS,
+        KC.LSFT,  KC.A,     KC.S,    KC.D,    KC.F,    KC.G,      KC.H,                             KC.J,      KC.K,      KC.L,      KC.O,      KC.A,      KC.ASTR,   KC.TRNS,
+        KC.LCTL,  KC.LABK,  KC.Z,    KC.X,    KC.C,    KC.V,      KC.B,                             KC.N,      KC.M,      KC.COMMA,  KC.DOT,    KC.SCLN,   KC.RSFT,   KC.TRNS,
+                                     KC.LCBR, KC.RCBR, KC.LSFT,   KC.SPC,  KC.MB_LMB,  KC.MB_RMB,                         KC.LALT,   KC.DEL,
+                                                                           KC.LGUI,    KC.TRNS,     KC.SPC,    KC.ENTER,  KC.BSPC,   KC.DEL
+    ],
+]
+
+oled_ext = Oled(
+    views=OledData(
+        corner_one={0:OledReactionType.STATIC, 1:["layer"]},
+        corner_two={0:OledReactionType.LAYER, 1:["1","2","3","4"]},
+        corner_three={0:OledReactionType.LAYER, 1:["base","raise","lower","adjust"]},
+        corner_four={0:OledReactionType.LAYER, 1:["qwerty","nums","shifted","leds"]},
+    ),
+    toDisplay=OledDisplayMode.TXT,
+    oWidth=128,
+    oHeight=32,
+    flip=True,
+)
+keyboard.extensions.append(oled_ext)
+
+if __name__ == '__main__':
+    keyboard.go()
